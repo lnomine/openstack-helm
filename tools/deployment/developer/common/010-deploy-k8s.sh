5eea2d684813e197495c479acb2c896b4e674ed8
Laurent Nominé <laurent.nomine@gmail.com>
2022-09-17 17:47:01 +0200

    Add br_netfilter

--- tools/deployment/developer/common/010-deploy-k8s.sh
+++ tools/deployment/developer/common/010-deploy-k8s.sh
@@ -14,6 +14,14 @@
 
 set -xe
 
+modprobe br_netfilter
+
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
 CURRENT_DIR="$(pwd)"
 : ${OSH_INFRA_PATH:="../openstack-helm-infra"}
 cd ${OSH_INFRA_PATH}
